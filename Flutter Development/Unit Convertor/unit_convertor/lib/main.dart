bae26fa6ced5cea4eef35506983519c084515587
Ghaem313Ghaem <mohammad.ali.alavi.313@gmail.com>
2023-12-19 12:54:58 +0330

    The convi splash screen is getting started.

--- Flutter Development/Unit Convertor/unit_convertor/lib/main.dart
+++ Flutter Development/Unit Convertor/unit_convertor/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+import 'screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +12,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +35,156 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 30000), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MainCalculationScreen(),
+        ),
+      );
     });
+
+    // _controller = VideoPlayerController.networkUrl(
+    //   Uri.parse(
+    //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    //   ),
+    // );
+
+    _controller = VideoPlayerController.asset("./assets/videos/Flutter.mp4")
+      ..initialize().then((_) {
+        setState(() {
+          _controller.play();
+        }); //here you could use Provider or any other state management approach. I use bloc
+      });
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    // _videoPlayerController =
+    //     VideoPlayerController.asset("./assets/videos/Drop_Splitting.mp4")
+    //       ..addListener(() => setState(() {}))
+    //       ..initialize().then((_) {
+    //         _videoPlayerController.setVolume(0.0);
+    //         _videoPlayerController.play();
+    //         _videoPlayerController.setLooping(true);
+    //         setState(() {});
+    //       });
+    // _videoPlayerController.initialize().then((_) {
+    //   // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+    //   setState(() {});
+    // });
+
+    // _chewieController = ChewieController(
+    //   videoPlayerController: _videoPlayerController,
+    //   aspectRatio: 15 / 6,
+    //   autoPlay: true,
+    //   looping: true,
+    //   showControls: true, // Hide the controls
+    //   // Errors can occur for example when trying to play a video
+    //   // from a non-existent URL
+    //   errorBuilder: (context, errorMessage) {
+    //     return Center(
+    //       child: Text(
+    //         errorMessage,
+    //         style: const TextStyle(color: Colors.white),
+    //       ),
+    //     );
+    //   },
+    // );
+
+    // _videoPlayerController.setLooping(true);
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: const Color.fromARGB(255, 255, 191, 0),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Image.asset(
+              "./assets/images/Logo.png",
+              width: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? MediaQuery.of(context).size.width * 0.7
+                  : MediaQuery.of(context).size.height * 0.7,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Convi",
+              style: TextStyle(
+                fontFamily: "LilitaOne",
+                fontSize:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? MediaQuery.of(context).size.width * 0.17
+                        : MediaQuery.of(context).size.height * 0.17,
+              ),
+            ),
+            Text(
+              "Easy Unit Converter",
+              style: TextStyle(
+                fontFamily: "SegoeUI",
+                fontSize:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? MediaQuery.of(context).size.width * 0.08
+                        : MediaQuery.of(context).size.height * 0.08,
+                letterSpacing: -2,
+                color: Colors.black,
+              ),
+            ),
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
     );
   }
 }
---
+++ Flutter Development/Unit Convertor/unit_convertor/lib/screens/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainCalculationScreen extends StatefulWidget {
+  const MainCalculationScreen({super.key});
+
+  @override
+  State<MainCalculationScreen> createState() => _MainCalculationScreenState();
+}
+
+class _MainCalculationScreenState extends State<MainCalculationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
